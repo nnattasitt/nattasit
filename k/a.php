a7d90ca82e918153a714973c7f65ac1c9b0eda93
nattasit <pondnattasit@gmail.com>
2026-02-18 16:55:09 +0700

    1

---
+++ k/a.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ณัฐสิทธิ์ พุฒธรรม (ปอนด์)</title>
+</head>
+
+<body>
+
+<h1> งาน K -- 66010910979 ณัฐสิทธิ์ พุฒธรรม (ปอนด์) </h1>
+
+<button 
+onclick="window.location.href='1.jpg'"
+style="background-color: green; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">
+เปิดรูปตัวเอง
+</button>
+
+<button 
+onclick="window.location.href='2.jpg'"
+style="background-color: #FFC107; color: black; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">
+เปิดรูปอาจารย์
+</button>
+
+</body>
+</html>
